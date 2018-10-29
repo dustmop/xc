01e063ffeac1aa6d53b3f9611b005923fd1498f9
Dustin Long <me@dustmop.io>
2018-10-29 16:26:09 -0400

    Multiple arguments, pad based upon widest value

--- xc.py
+++ xc.py
@@ -1,16 +1,28 @@
 import sys
 
 
+def show(text):
+  if text[:2] == '0x':
+    return int(text[2:], 16)
+  else:
+    return int(text)
+
+
 def run():
   if len(sys.argv) < 2:
     print('Usage: xc <num>')
     sys.exit(1)
-  arg = sys.argv[1]
-  if arg[:2] == '0x':
-    num = int(arg[2:], 16)
-  else:
-    num = int(arg)
-  print('0x%x   %d' % (num, num))
+  max_width = 0
+  for n in xrange(1, len(sys.argv)):
+    arg = sys.argv[n]
+    text = '%x' % show(arg)
+    if len(text) > max_width:
+      max_width = len(text)
+  print_template = '0x%0' + str(max_width) + 'x   %d'
+  for n in xrange(1, len(sys.argv)):
+    arg = sys.argv[n]
+    num = show(arg)
+    print(print_template % (num, num))
 
 
 if __name__ == '__main__':
