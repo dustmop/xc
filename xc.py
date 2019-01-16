ac53e775981081e4ea4c2c303cdfe81d1f429d13
Dustin Long <me@dustmop.io>
2019-01-16 13:30:39 -0500

    Allow additions from command line

--- xc.py
+++ xc.py
@@ -1,28 +1,53 @@
 import sys
 
 
-def show(text):
-  if text[:2] == '0x':
-    return int(text[2:], 16)
+def collect_values(args):
+  oper = None
+  vals = []
+  for a in args:
+    if oper:
+      if oper == '+':
+        left = vals.pop()
+        right = parse_value(a)
+        vals.append(left + right)
+      oper = None
+      continue
+    if a == '+':
+      oper = a
+    elif a == ',':
+      pass
+    else:
+      vals.append(parse_value(a))
+  return vals
+
+
+def parse_value(arg):
+  if arg[0] == 'x':
+    return int(arg[1:], 16)
+  elif arg[0:2] == '0x':
+    return int(arg[2:], 16)
   else:
-    return int(text)
+    return int(arg, 10)
+
+
+def get_max_width(vals):
+  max_width = 0
+  for n in vals:
+    text = '%d' % n
+    if len(text) > max_width:
+      max_width = len(text)
+  return max_width
 
 
 def run():
   if len(sys.argv) < 2:
     print('Usage: xc <num>')
     sys.exit(1)
-  max_width = 0
-  for n in xrange(1, len(sys.argv)):
-    arg = sys.argv[n]
-    text = '%x' % show(arg)
-    if len(text) > max_width:
-      max_width = len(text)
-  print_template = '0x%0' + str(max_width) + 'x   %d'
-  for n in xrange(1, len(sys.argv)):
-    arg = sys.argv[n]
-    num = show(arg)
-    print(print_template % (num, num))
+  vals = collect_values(sys.argv[1:])
+  max_width = get_max_width(vals)
+  print_template = '0x%0' + str(max_width) + 'x   %' + str(max_width) + 'd'
+  for n in vals:
+    print(print_template % (n, n))
 
 
 if __name__ == '__main__':
