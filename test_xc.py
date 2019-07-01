77a9a40bc0cc417d035a2502f6adc3cb903ddf3f
Dustin Long <me@dustmop.io>
2019-06-30 22:40:58 -0400

    Unit tests

---
+++ test_xc.py
@@ -0,0 +1,74 @@
+import unittest
+
+import xc
+
+
+class XCTests(unittest.TestCase):
+  def test_basic(self):
+    (vals, errs) = xc.collect_values(['123'])
+    self.assertEqual(errs, [])
+    self.assertEqual(vals, [123])
+
+  def test_multiple_values(self):
+    (vals, errs) = xc.collect_values(['34', '56', '78'])
+    self.assertEqual(errs, [])
+    self.assertEqual(vals, [34, 56, 78])
+
+  def test_ignore_comma(self):
+    (vals, errs) = xc.collect_values(['34', ',', '56,', '78'])
+    self.assertEqual(errs, [])
+    self.assertEqual(vals, [34, 56, 78])
+
+  def test_addition(self):
+    (vals, errs) = xc.collect_values(['203', '+', '142'])
+    self.assertEqual(errs, [])
+    self.assertEqual(vals, [345])
+
+  def test_subtraction(self):
+    (vals, errs) = xc.collect_values(['203', '-', '142'])
+    self.assertEqual(errs, [])
+    self.assertEqual(vals, [61])
+
+  def test_multiply(self):
+    (vals, errs) = xc.collect_values(['203', '*', '142'])
+    self.assertEqual(errs, [])
+    self.assertEqual(vals, [28826])
+
+  def test_divide(self):
+    (vals, errs) = xc.collect_values(['564', '/', '2'])
+    self.assertEqual(errs, [])
+    self.assertEqual(vals, [282.0])
+
+  def test_exponent(self):
+    (vals, errs) = xc.collect_values(['24', '**', '5'])
+    self.assertEqual(errs, [])
+    self.assertEqual(vals, [7962624])
+
+  def test_hex(self):
+    (vals, errs) = xc.collect_values(['0xabc', 'x124', '0x10', 'xc84'])
+    self.assertEqual(errs, [])
+    self.assertEqual(vals, [2748, 292, 16, 3204])
+
+  def test_negative(self):
+    (vals, errs) = xc.collect_values(['-564'])
+    self.assertEqual(errs, [])
+    self.assertEqual(vals, [-564])
+
+  def test_negative_hex(self):
+    (vals, errs) = xc.collect_values(['-0x34', '-x1234'])
+    self.assertEqual(errs, [])
+    self.assertEqual(vals, [-52, -4660])
+
+  def test_floating(self):
+    (vals, errs) = xc.collect_values(['35.7'])
+    self.assertEqual(errs, [])
+    self.assertEqual(vals, [35.7])
+
+
+# TODO:
+# padding
+# display hex and dec, correct width
+
+
+if __name__ == '__main__':
+  unittest.main()
